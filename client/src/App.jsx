8644dd6847ff3de495c8b72bb98e52c8b7f85982
Sush25N <sushgabhane123@gmail.com>
2024-04-23 17:51:05 +0530

    initialized a project with login.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./Login";
+
+function App() {
+  return (
+    <Router>
+      <Login />
+    </Router>
+  );
+}
+
+export default App;
